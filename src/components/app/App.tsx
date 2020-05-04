56a149668a9e66802780aa8e0f6ccacf3ec08390
muramoto haruka <xapyka@muramotonoMacBook-ea.local>
2020-05-04 22:32:58 +0900

    Setting for routing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Top from '../top/top'
+import signUp from '../sign-up/sign-up';
+import signIn from '../sign-in/sign-in';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Top />
+    <BrowserRouter>
+      <switch>
+        <Route exact path="/" component={Top} />
+        <Route path='/signin' component={signIn} />
+        <Route path='/signup' component={signUp} />
+      </switch>
+    </BrowserRouter>
   )
 }
 
--- src/components/top/top.tsx
+++ src/components/top/top.tsx
@@ -1,40 +1,32 @@
 import React from 'react';
-import { Link, BrowserRouter, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../app/App.css';
-import signUp from '../sign-up/sign-up';
-import signIn from '../sign-in/sign-in';
 
 function Top() {
     return (
-        <BrowserRouter>
-            <div>
-                <header className="App-header">
-                    <img src="/logo.png" className="App-logo" alt="logo" />
-                    <a
-                        className="App-link"
-                        href="/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                    </a>
-                    <Link to="/signin">
-                        <button className="App-sign-in App-button">
-                            Sign In
+        <div>
+            <header className="App-header">
+                <img src="/logo.png" className="App-logo" alt="logo" />
+                <a
+                    className="App-link"
+                    href="/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+                <Link to="/signin">
+                    <button className="App-sign-in App-button">
+                        Sign In
                         </button>
-                    </Link>
-                    <Link to="/signup">
-                        <button className="App-sign-up App-button">
-                            Sign Up
+                </Link>
+                <Link to="/signup">
+                    <button className="App-sign-up App-button">
+                        Sign Up
                         </button>
-                    </Link>
-                </header>
-                <switch>
-                    <Route path='/signin' component={signIn} />
-                    <Route path='/signup' component={signUp} />
-                </switch>
-            </div>
-        </BrowserRouter>
+                </Link>
+            </header>
+        </div>
     )
 }
 
-export default Top
+export default Top;
