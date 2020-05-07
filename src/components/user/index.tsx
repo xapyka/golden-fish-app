4233e58129f72593737ba23cf35a2afc363326d2
muramoto haruka <xapyka@muramotonoMacBook-ea.local>
2020-05-08 00:07:42 +0900

    Add header and upload button

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppHeader from '../top/header';
+import ImageUpLoader from './image-uploader';
 
 function UserPage() {
     return (
         <div>
             <header className="App-header">
                 <AppHeader />
+                <ImageUpLoader />
             </header>
         </div>
     )
